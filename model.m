bb1bf7494c971dcbece027db33a958d205d3d5b3
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-08-03 13:43:58 -0700

    f

--- model.m
+++ model.m
@@ -1,15 +1,12 @@
 % time taken for a tank to drain
 % length of pipe segment
 Length={0.2,0.3,0.4,0.6};
-FrictionFactor={0.6,0.6,0.6,0.06};
+FrictionFactor=0.00;
 % initial height of water
 Z_1=0.08;
 % water left in tank after drainage
 Z_2=0.02;
-% vertical drop of the pipe
-Z_3=L/150;
 % total height
-Z_tot=Z_1+Z_2+Z_3;
 % cross-sectional area of tank
 A_tank=0.32*0.26;
 % diameter of pipe
@@ -30,11 +27,14 @@ u=1.003E-3;
 for L=1:length(Length)
     disp('---------')
     disp(Length{L});
+    % vertical drop of the pipe
+    Z_3=Length{L}/150;
+    Z_tot=Z_1+Z_2+Z_3;
     Re=(roe*sqrt((2*g)*((Length{L}/150)+Z_1+Z_2)*d))/u;
     A=2*sqrt(Z_2+Z_3);
     B=2*sqrt(Z_tot);
     C=(-1)*(A_pipe/A_tank);
-    D=sqrt((2*g)/(1+(((Length{L}*FrictionFactor{L})/d)+k)));
+    D=sqrt((2*g)/(1+(((Length{L}*FrictionFactor)/d)+k)));
     t=(A-B)/C*D;
     disp(t/60);
     disp(Re)
